a81e8243022a7af0936a8964cf6328d4e07af2c6
Moses <mutisyamoses722@gmail.com>
2024-06-09 15:41:02 +0300

    adding magazines to the table

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 from models.author import Author
+from models.magazine import Magazine
 
 Author.drop_table()
 Author.create_table()
+
+Magazine.drop_table()
+Magazine.create_table()
 
 new_author= Author.create('John Doe')
 print("Author ID:", new_author.id)
@@ -12,3 +16,7 @@ print("New Author ID:", new_author2.id)
 print("New Author Name:", new_author2.name)
 
 new_author3 =Author.create('Moses Mutisya')
+
+new_magazine=Magazine.create('Headlines', 'Breaking News')
+new_magazine2= Magazine.create("Apple's secret", 'Health' )
+new_magazine3 = Magazine.create('Moringa school', 'Education')
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,42 @@
+from database.connection import get_db_connection
+from database.setup import create_tables, drop_tables
+
 class Magazine:
-    def __init__(self, id, name, category):
+    def __init__(self, name, category, id=None):
         self.id = id
         self.name = name
         self.category = category
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
+    
+    @classmethod
+    def create_table(cls):
+        create_tables()
+
+    @classmethod
+    def drop_table(cls):
+        drop_tables()
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        sql = """
+            INSERT INTO magazines (
+            name, category
+            ) VALUES (?, ?)
+        """
+
+        cursor.execute(sql, (self.name, self.category,))
+        conn.commit()
+
+        self.id = cursor.lastrowid
+
+
+    @classmethod
+    def create(cls, name, category):
+        Magazine = cls(name, category)
+        Magazine.save()
+
+        return Magazine
