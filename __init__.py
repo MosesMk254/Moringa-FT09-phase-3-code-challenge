dd658b75fbc2a41ef8cd05a6a7307e09b24376c1
Moses <mutisyamoses722@gmail.com>
2024-06-09 18:42:05 +0300

    adding articles to the db

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from models.author import Author
 from models.magazine import Magazine
+from models.article import Article
 
 Author.drop_table()
 Author.create_table()
 
 Magazine.drop_table()
 Magazine.create_table()
+
+Article.drop_table()
+Article.create_table()
 
 new_author= Author.create('John Doe')
 print("Author ID:", new_author.id)
@@ -20,3 +24,7 @@ new_author3 =Author.create('Moses Mutisya')
 new_magazine=Magazine.create('Headlines', 'Breaking News')
 new_magazine2= Magazine.create("Apple's secret", 'Health' )
 new_magazine3 = Magazine.create('Moringa school', 'Education')
+
+new_article = Article.create('People', 'Education', new_author, new_magazine)
+new_article2 = Article.create('School', 'Moringa', new_author3, new_magazine2)
+new_article3 = Article.create('Taste', 'Food', new_author2, new_magazine3)
--- models/article.py
+++ models/article.py
@@ -1,5 +1,8 @@
+from database.connection import get_db_connection
+from database.setup import create_tables, drop_tables
+
 class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
+    def __init__(self, title, content, author_id, magazine_id, id=None):
         self.id = id
         self.title = title
         self.content = content
@@ -8,3 +11,69 @@ class Article:
 
     def __repr__(self):
         return f'<Article {self.title}>'
+
+
+    @classmethod
+    def create_table(cls):
+        create_tables()
+
+    @classmethod
+    def drop_table(cls):
+        drop_tables()
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        sql = """
+            INSERT INTO articles (
+            title, content, author_id, magazine_id
+            ) VALUES (?, ?, ?, ?)
+        """
+
+        cursor.execute(sql, (self.title, self.content, self.author_id, self.magazine_id,))
+        conn.commit()
+
+        self.id = cursor.lastrowid
+
+    @classmethod
+    def create(cls, title, content, author, magazine):
+        article = cls(title, content, author.id, magazine.id)
+        article.save()
+
+        return article
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        if value is not None and not isinstance(value, int):
+            raise ValueError("ID must be of type int")
+        self._id = value
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Title must be a string")
+        if not 5 <= len(value) <= 50:
+            raise ValueError("Title must be between 5 and 50 characters")
+        self._title = value
+
+    @property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Content must be a string")
+        if hasattr(self, '_name'):
+            raise AttributeError("Article cannot be changed after instantiation")
+        self._content = value
+
